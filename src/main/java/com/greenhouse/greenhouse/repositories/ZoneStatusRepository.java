2b64228abece8552be253290350d55e7049794a2
Sconguser <scongi.30@gmail.com>
2025-05-12 14:39:08 +0200

    delete zone status

--- src/main/java/com/greenhouse/greenhouse/repositories/ZoneStatusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greenhouse.greenhouse.repositories;
-
-import com.greenhouse.greenhouse.models.ZoneStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ZoneStatusRepository extends JpaRepository<ZoneStatus, Long> {
-}
